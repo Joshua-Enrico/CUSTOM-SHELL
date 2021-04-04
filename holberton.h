a45131e8ecb8da74fa9e81efbd4076d174f986b8
yoyogold-a11 <tmrfack@gmail.com>
2021-04-04 00:59:57 +0000

    holberton.h

---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_HOLBERTON_
+#define _SHELL_HOLBERTON_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
+
+/******Some String operations and manipulation******/
+char *_strdup(char *strtodup);
+
+
+#endif /* _SHELL_HOLBERTON_ */
