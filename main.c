11023cccf161358d36ea688d596c6c6affda8431
yoyogold-a11 <tmrfack@gmail.com>
2021-04-07 07:32:47 +0000

    making few changes

--- main.c
+++ main.c
@@ -3,28 +3,26 @@
 int main(int argc __attribute__((unused)), char **argv, char **environment __attribute__((unused)))
 {
 	size_t len_buffer = 0;
- 
 
 	vars_t vars = {NULL, NULL, 0, NULL, 0};
 
 	vars.argv = argv;
 
-
 	_puts("$ ");
 
 	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
 	{
-		vars.array_tokens = tokenizer(vars.buffer, " ");
+		vars.counter++;
+		vars.array_tokens = tokenizer(vars.buffer, " \t\r\n\a");
 
+		if (vars.array_tokens && vars.array_tokens[0])
+			if (check_for_builtins(&vars) == NULL)
+		
 
-
-		if (check_for_builtins(&vars) == NULL)
-		{
-
-		}
+		free(vars.buffer);
 		_puts("$ ");
+		vars.buffer = NULL;
 	}
 
-
-		exit(vars.status);
+	exit(vars.status);
 }
