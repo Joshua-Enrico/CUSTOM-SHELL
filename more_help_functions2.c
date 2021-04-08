c3feada85f5dc49cd6779dda0b79e796e3bebcdf
yoyogold-a11 <tmrfack@gmail.com>
2021-04-08 23:06:59 +0000

    tweaking help function

--- more_help_functions2.c
+++ more_help_functions2.c
@@ -3,11 +3,11 @@
 void new_help10(vars_t *vars)
 {
 
-    prints_error_msg(vars, ": no help topics match: ");
+    prints_error_msg(vars, ": no help topics match: `");
     _puts_error(vars->array_tokens[1]);
     
 
-    _puts_error(" Try `help help' or `man -k ");
+    _puts_error("' Try `help help' or `man -k ");
       _puts_error(vars->array_tokens[1]);
       _puts_error("' or `info ");
        _puts_error(vars->array_tokens[1]);
